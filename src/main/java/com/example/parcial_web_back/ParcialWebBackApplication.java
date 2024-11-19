481f4762ff9c1db0f49194e5bf94c0e993f1b8f3
Andrés Ruge <119058759+Rujnp794@users.noreply.github.com>
2024-11-19 10:33:08 -0500

    first commit

---
+++ src/main/java/com/example/parcial_web_back/ParcialWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcial_web_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialWebBackApplication.class, args);
+	}
+
+}
